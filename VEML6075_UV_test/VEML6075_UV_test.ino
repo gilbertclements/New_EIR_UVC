1b9890ab613b2abcf354e8cbd55e5bb4cb45c8f6
gilbertclements <62492240+gilbertclements@users.noreply.github.com>
2021-03-31 16:59:31 -0700

    Update VEML6075_UV_test.ino

--- VEML6075_UV_test/VEML6075_UV_test.ino
+++ VEML6075_UV_test/VEML6075_UV_test.ino
@@ -13,7 +13,7 @@
 VEML6075 UV_Sensor;                                                     // Create a VEML6075 object
 
 void setup(){ ///////////////////////setup////////////////////////////
-  Serial.begin(115200);                                                 // baud rate serial monitor
+  Serial.begin(115200);                                                 //Communicate with serial montor
   Wire.begin();
   if (!UV_Sensor.begin()) {                                             // confirm communication on serial monitor
     Serial.println("Unable to communicate with VEML6075. Check Quiic connection.");
@@ -21,11 +21,10 @@ void setup(){ ///////////////////////setup////////////////////////////
   }
   Serial.print("Raw UVA: "); Serial.println(UV_Sensor.rawUva());
   Serial.print("Raw UVB: "); Serial.println(UV_Sensor.rawUvb());
-  Serial.print("Raw IR:  ");  Serial.println(UV_Sensor.irCompensation());
-  Serial.print("UVA:\t");     Serial.println(UV_Sensor.a());
-  Serial.print("UVB:\t");     Serial.println(UV_Sensor.b());
-  Serial.print("UVIndex:\t"); Serial.println(UV_Sensor.index());
-  Serial.println("UVA,\tUVB,\tUV Index");
+  Serial.print("Raw IR: ");  Serial.println(UV_Sensor.irCompensation());
+  Serial.print("UVA:\t");    Serial.println(UV_Sensor.a());
+  Serial.print("UVB:\t");    Serial.println(UV_Sensor.b());
+  Serial.print("UVIndex:");  Serial.println(UV_Sensor.index());
   
   pinMode(LED_BUILTIN, OUTPUT);                                                       // initialize the LED pin as an output, LED pin 13 on ESP32:
   for (byte count = 0; count < 5; count++) {                                          // short strobe confirm i2c communication
@@ -36,12 +35,14 @@ void setup(){ ///////////////////////setup////////////////////////////
 }
 
 void loop(){ ///////////////////////loop////////////////////////////
-  // Use the uva, uvb, and index functions to read calibrated UVA and UVB values and a
-  // calculated UV index value between 0-11.
-  Serial.print(UV_Sensor.uva());
-  Serial.print("\t");
-  Serial.print(UV_Sensor.uvb());
-  Serial.print("\t");
-  Serial.println(UV_Sensor.index());
-  delay(1000*5);  //Wait 5 second
+  // Use the uva, uvb, and index functions to read calibrated UVA and UVB values and a calculated UV index value between 0-11.
+  Serial.println(F("\nUVA\tUVB\tUVIndex"));            // Show header    
+  for (byte count = 0; count < 15; count++) {                                            // Loop
+    Serial.print(UV_Sensor.uva());
+    Serial.print("\t");
+    Serial.print(UV_Sensor.uvb());
+    Serial.print("\t");
+    Serial.println(UV_Sensor.index());
+    delay(1000*5);  //Wait 5 second
+  }
 }
