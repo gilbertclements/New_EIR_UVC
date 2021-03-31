73cacc0f725e854ea29d03ade73afe9634459a4e
gilbertclements <62492240+gilbertclements@users.noreply.github.com>
2021-03-31 16:58:08 -0700

    Update SGP3_IAQ_test.ino

--- SGP3_IAQ_test/SGP3_IAQ_test.ino
+++ SGP3_IAQ_test/SGP3_IAQ_test.ino
@@ -13,19 +13,19 @@
 SGP30 SGP30_Sensor;                                                                  //create an object of the SGP30 
 
 void setup() {  ///////////////////////setup////////////////////////////
-  Serial.begin(115200);                                                              // baud rate serial monitor
+  Serial.begin(115200);
   Wire.begin();
   if (!SGP30_Sensor.begin()) {
     Serial.println("Unable to communicate with SGP30. Check Quiic connection.");
     while (1);
   }
   SGP30_Sensor.initAirQuality();
-  SGP30_Sensor.measureRawSignals();                                                  //get raw values for H2 and Ethanol
+  SGP30_Sensor.measureRawSignals();  //get raw values for H2 and Ethanol
   Serial.print("Raw H2: ");        Serial.print(SGP30_Sensor.H2);
   Serial.print("\tRaw Ethanol: "); Serial.println(SGP30_Sensor.ethanol);
-  SGP30_Sensor.measureAirQuality();                                                  //measure CO2 and TVOC levels
-  Serial.print("CO2: ");           Serial.print(SGP30_Sensor.CO2);
-  Serial.print(" ppm\tTVOC: ");    Serial.print(SGP30_Sensor.TVOC);Serial.println(" ppb");
+  SGP30_Sensor.measureAirQuality();  //measure CO2 and TVOC levels
+  Serial.print("CO2(ppm): ");      Serial.print(SGP30_Sensor.CO2);
+  Serial.print("\tTVOC(ppb): ");   Serial.print(SGP30_Sensor.TVOC);
  
   pinMode(LED_BUILTIN, OUTPUT);                                                       // initialize the LED pin as an output, LED pin 13 on ESP32:
   for (byte count = 0; count < 5; count++) {                                          // short strobe confirm i2c communication
@@ -36,10 +36,12 @@ void setup() {  ///////////////////////setup////////////////////////////
 }
 
 void loop() { ///////////////////////loop////////////////////////////
-  //First fifteen readings will be baseline CO2: 400 ppm  TVOC: 0 ppb
-  //measure CO2 and TVOC levels
-  SGP30_Sensor.measureAirQuality();
-  Serial.print("CO2: ");           Serial.print(SGP30_Sensor.CO2);
-  Serial.print(" ppm\tTVOC: ");    Serial.print(SGP30_Sensor.TVOC); Serial.println(" ppb");
+  //First fifteen readings will be baseline CO2: 400 ppm  TVOC: 0 ppb measure CO2 and TVOC levels
+  Serial.println(F("\nCO2(ppm) TVOC(ppb)"));            // Show header    
+  for (byte count = 0; count < 15; count++) {                                            // Loop
+    Serial.print(SGP30_Sensor.CO2);
+    Serial.print("\t ");
+    Serial.println(SGP30_Sensor.TVOC);
   delay(1000*5); //Wait 5 second
+  }
 }
